41d9f639e1f278a8fc2752d32d3ade1f7c4eac76
Victor Manuel Echavarria <vicme93@gmail.com>
2019-12-03 20:24:49 -0500

    trabajo final php

---
+++ pagina.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <title>
+          DIM 106 AÑOS  
+        </title>
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+        <div id="pagewrap">
+                <img id="logo1" src="images/escudo.png">
+                <img id="logo2" src="images/escudo.png">
+    <center>
+        <div id="header">
+
+                <h1> DIM 106 AÑOS</h1>
+                <p>Inicio - Mision - Vision - Contactenos</p>
+                
+
+        </div>
+    </center>
+<div id="content">
+            <h2>Historia</h2>
+            <p>El 14 de noviembre de 1913 Alberto Uribe Piedrahíta y sus hermanos Luis y Rafael, acompañados por un grupo de paisas provenientes de Familias pudientes, tales como Guillermo Greiffenstein y José Luis Jaramillo, se entusiasmaron con la idea de crear un equipo de fútbol organizado que se enfrentara a Sporting, un equipo paisa fundado en 1913 por un grupo de antioqueños y europeos, en especial de belgas y suizos, se forjaron iniciativas, se establecieron contactos para el funcionamiento del equipo y se conforma un equipo llamado Medellín FootBall Club.</p>
+        
+            <h2>Nuestro Escudo</h2>
+            <p>El primer escudo que caracterizó al Deportivo Independiente Medellín data de 1945, similar al de San Lorenzo de Argentina, con una franja diagonal en la mitad que decía «Medellín» y en su parte inferior en un círculo: "F.B.C", que fue el primer nombre que tuvo el club.</p>
+
+        <div id="valor">
+            <h2>Rivalidad</h2>
+            Es el partido que enfrenta a los dos principales equipos del Valle de Aburrá y propiamente los únicos clubes profesionales de la
+             ciudad de Medellín: Atlético Nacional e Independiente Medellín. Es uno de los clásicos tradicionales del fútbol colombiano, El primer clásico que se disputó en la historia entre ambos equipos fue el 12 de septiembre de 1948 y la victoria se la llevó el Independiente Medellín superando al que en el momento se denominaba Atlético Municipal
+        </div>            
+        
+        <div id="lista">
+            <h2>Lista de titulos </h2>
+            
+                <p>
+                liga colombiana Profesional: 6 titulos
+            
+                <p>    
+                liga colombiana Amateur : 16 titulos
+                <p>    
+                copa colombia: 2 titulos 
+                
+            </p>
+        </div>
+</div>
+
+        <div id="sidebar1">
+
+            <h3>El Maximo Idolo</h3>
+            <p> Jose Manuel Moreno, pasó por Independiente Medellín, donde su último partido lo jugó en 1961, a los 44 años, para el equipo colombiano -que también dirigía-, en un amistoso ante Boca Juniors: ganaron 5 a 2 con dos goles suyos. Cinco minutos antes de finalizar el partido, sin anticipar ni avisar nada, levantó los brazos, saludó al público y se fue. Era el fin de la carrera de un grande de todos los tiempos.</p>
+        </div>
+
+        <div id="sidebar2">
+
+            <h3>primer titulo:</h3>
+            <p>
+            En 1955​ se continuó con el buen proceso que se llevaba desde el año anterior y los pronósticos no fallaron. Esta primera estrella comenzó a tejerse el 24 de abril, las buenas presentaciones del Independiente Medellín continuaron con buen fútbol, goles y triunfos. En la primera ronda el equipo ganó 9 partidos de 9, con 3 goleadas memorables; al Tolima, Cúcuta y Millonarios, el Poderoso barrió rivales de principio a fin. El primer título se dio el 23 de octubre en la ciudad de Cúcuta ante 5000 espectadores. 
+        </p>
+        
+        </div>
+<center>
+        <div id="footer">
+                <img id="botella1" src="images/dim.png">
+                <img id="botella2" src="images/dim.png">
+        <p>
+            Contactenos:
+            
+            <p>
+            dimoficial.com
+
+            <p>
+               Víctor Manuel Echavarría
+            <p>
+                Analisis y Desarrollo de Software - censa
+        </p>
+        
+        </div>
+</center>
+
+</div>
+
+        
+    </body>
+</html>
